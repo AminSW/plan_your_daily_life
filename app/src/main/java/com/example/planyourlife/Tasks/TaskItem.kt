4c96bb86d866d7a13f3169fab0831f2fd5b24f20
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-12-13 21:21:59 +0300

    Creating TaskItem SuperClass

---
+++ app/src/main/java/com/example/planyourlife/Tasks/TaskItem.kt
@@ -0,0 +1,12 @@
+package com.example.planyourlife.Tasks
+
+import androidx.compose.runtime.Composable
+
+open class TaskItem
+{
+    @Composable
+    open fun showDetails()
+    {
+        TODO("Not yet implemented")
+    }
+}
