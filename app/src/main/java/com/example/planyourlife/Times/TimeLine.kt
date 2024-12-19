dd05346d5255a3ca94ca3ca5c8b0376f10ff4a8a
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-12-19 23:37:21 +0300

    Head of TimeLine

--- app/src/main/java/com/example/planyourlife/Times/TimeLine.kt
+++ app/src/main/java/com/example/planyourlife/Times/TimeLine.kt
@@ -1,4 +1,27 @@
 package com.example.planyourlife.Times
 
-class TimeLine {
+class TimeLine(private val start: Float, private val end: Float)
+{
+    fun returnDuration(): ArrayList<Float> {
+        val duration = ArrayList<Float>()
+        var index = start
+
+        while (index <= end)
+        {
+            duration.add(index)
+            index++
+        }
+
+        return duration
+    }
+
+    fun getStartTime(): Float
+    {
+        return start
+    }
+
+    fun getEndTime(): Float
+    {
+        return end
+    }
 }
