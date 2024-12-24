183bf26865adac3a34a54f62fbf2ef5003d4ef55
Amin Azizzade <amin.azizcomp24@gmail.com>
2024-12-24 23:00:13 +0300

    Necessary changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //alias(libs.plugins.kotlin.serialization)
+
+    //id("androidx.navigation.safeargs")
 }
 
 android {
@@ -59,6 +62,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+//    implementation(libs.androidx.navigation.runtime.ktx)
+//    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +71,34 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation("androidx.lifecycle: lifecycle-viewmodel:2.5.1")
+    implementation("androidx.activity: activity-ktx:1.6.1")
+    implementation("androidx.compose.runtime: runtime-livedata:1.5.4")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+//    val nav_version = "2.8.0"
+//
+//    // Jetpack Compose integration
+//    implementation("androidx.navigation:navigation-compose:$nav_version")
+//
+//    // Views/Fragments integration
+//    implementation("androidx.navigation:navigation-fragment:$nav_version")
+//    implementation("androidx.navigation:navigation-ui:$nav_version")
+//
+//    // Feature module support for Fragments
+//    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+//
+//    // Testing Navigation
+//    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+//
+//    //val nav_version = "2.8.0"
+//    //classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
 }
