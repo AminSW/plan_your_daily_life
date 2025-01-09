dc34ac3ee77fbdaee494efd0ab8ed1c63e202c19
Amin Azizzade <amin.azizcomp24@gmail.com>
2025-01-09 20:57:49 +0300

    Database Creation

--- app/src/main/java/com/example/planyourlife/dependencyInjection/AppModule.kt
+++ app/src/main/java/com/example/planyourlife/dependencyInjection/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.planyourlife.dependencyInjection;
 
+import android.content.Context
+import androidx.room.Room
 import com.example.planyourlife.data.datasource.DailyPlanningDataSource;
 import com.example.planyourlife.data.repository.DailyPlanningRepository;
+import com.example.planyourlife.room.Database
+import com.example.planyourlife.room.TimeLineTaskDAO
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent;
 import javax.inject.Singleton;
 
@@ -19,10 +24,18 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideDailyPlanningDataSource(): DailyPlanningDataSource {
-        return DailyPlanningDataSource();
+    fun provideDailyPlanningDataSource(timeLineTaskDAO: TimeLineTaskDAO): DailyPlanningDataSource {
+        return DailyPlanningDataSource(timeLineTaskDAO);
     }
 
+    @Provides
+    @Singleton
+    fun provideTimeLineTaskDAO(@ApplicationContext context: Context): TimeLineTaskDAO
+    {
+        val database = Room.databaseBuilder(context, Database::class.java, "planyourlife.db")
+            .createFromAsset("planyourlife.db").build()
+        return database.getTimeLineTaskDAO()
+    }
 }
 
 
